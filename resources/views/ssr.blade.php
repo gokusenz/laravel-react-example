0771746e177ef7e46fec74b15e992514e457f455
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-05-08 18:38:28 +0700

    add files

---
+++ resources/views/ssr.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Laravel 5.4 & React</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="https://bootswatch.com/cosmo/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<div id="app">12345</div>
+</body>
+</html>
